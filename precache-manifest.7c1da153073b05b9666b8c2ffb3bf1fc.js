4f8d130af05c8a4082a429646a98e9dab3469624
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-10-31 15:50:29 -0500

    Updates

---
+++ precache-manifest.7c1da153073b05b9666b8c2ffb3bf1fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636f465d5a2c028923c65cc537c1f9e7",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "b8b7ca9e230ab21b5365",
+    "url": "/weather/static/css/2.d58fcd6a.chunk.css"
+  },
+  {
+    "revision": "6948754856d7bfbeba93",
+    "url": "/weather/static/css/main.e96e9a55.chunk.css"
+  },
+  {
+    "revision": "b8b7ca9e230ab21b5365",
+    "url": "/weather/static/js/2.35d7f728.chunk.js"
+  },
+  {
+    "revision": "6948754856d7bfbeba93",
+    "url": "/weather/static/js/main.87059590.chunk.js"
+  },
+  {
+    "revision": "9c57c8b875a463e62dba",
+    "url": "/weather/static/js/runtime-main.d2a4a5e3.js"
+  },
+  {
+    "revision": "58f2f1fbfb935b5274743895f588488a",
+    "url": "/weather/static/media/weather_logo.58f2f1fb.png"
+  }
+]);
